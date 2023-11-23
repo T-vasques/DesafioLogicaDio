fbe9277b7b6a6a3909c4ed519b3a52d401806fc9
T-vasques <tvasques02@gmail.com>
2023-11-22 23:57:12 -0400

    ClassificaçãoDeHeroi

---
+++ index.js
@@ -0,0 +1,21 @@
+let NomeHeroi = "Tux"
+let LvlHeroi = 8500
+
+if (LvlHeroi <=1000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Ferro")
+}else if (LvlHeroi <=2000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Bronze")
+}else if (LvlHeroi <=5000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Prata")
+}else if (LvlHeroi <=7000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Ouro")
+}else if (LvlHeroi <=8000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Platina Diamante")
+}else if (LvlHeroi <=9000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Ascendente")
+}else if (LvlHeroi <=10000){
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Imortal")
+}
+else{
+    console.log("O Herói de nome "+NomeHeroi+" está no nível "+LvlHeroi+" Radiante")
+}
